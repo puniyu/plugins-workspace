a43de7e58ee17f806ca904916b2c7e18ea10c0da
wuliya <3369906077@qq.com>
2025-11-29 23:26:27 +0800

    chore(puniyu_plugin_echo): 更新新版参数使用方式

--- packages/puniyu_plugin_echo/src/echo.rs
+++ packages/puniyu_plugin_echo/src/echo.rs
@@ -3,10 +3,10 @@ use puniyu_plugin::prelude::*;
 #[command(
     name = "echo", 
     desc = "输出一段文本",
-    args = ["msg"]
+    args = [("msg")]
 )]
 async fn echo(bot: &BotContext, ev: &MessageContext) -> HandlerResult {
-    if let Some(msg) = ev.arg("msg") && !msg.is_empty() {
+    if let Some(msg) = ev.arg("msg").and_then(|v| v.as_str()) {
         bot.reply(msg.into()).await?;
     }
     Ok(().into())
